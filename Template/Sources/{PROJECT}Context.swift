5d1c0e7c14257d2ab5f06af9fdfaaafeeae00818
Simone Civetta <viteinfinite@gmail.com>
2018-08-28 10:32:17 +0200

    Update architecture definition

--- Template/Sources/{PROJECT}Context.swift
+++ Template/Sources/{PROJECT}Context.swift
@@ -2,11 +2,11 @@ import Foundation
 import Core
 
 public class {PROJECT}Context: BaseContext<{PROJECT}State, {PROJECT}Action> {
-    var routingRequestHandler: RoutingRequestHandler<{PROJECT}Route> {
-        return dependencyContainer.resolve(RoutingRequestHandler<{PROJECT}Route>.self)!
+    var moduleOutputHandler: ModuleOutputHandler<{PROJECT}Module.Output> {
+        return dependencyContainer.resolve(ModuleOutputHandler<{PROJECT}Module.Output>.self)!
     }
     
-    public override func reduce(state: {PROJECT}State, action: {PROJECT}Action) -> {PROJECT}State {
+    public override func reduce(state: {PROJECT}State, action: {PROJECT}Action) -> Reduction<{PROJECT}State, {PROJECT}Action> {
     
     }
 }
--- Template/Sources/{PROJECT}Module.swift
+++ Template/Sources/{PROJECT}Module.swift
@@ -3,25 +3,35 @@ import Core
 import UIKit
 import Swinject
 
-public enum {PROJECT}Route: Route {
-    case show
-}
-
-public class {PROJECT}Module: ModuleDefining, PresentableCreator, UserActivityRouteTransformer {
-    public let moduleKind = // TODO
+public class {PROJECT}Module: PresentableCreator, UserActivityRouteTransformer {
+    public enum Route: ModuleRoute {
+        case show
+    }
+    
+    public enum Output: ModuleOutput {
+        case dismiss
+    }
+    
     private let dependencyContainer: Container
 
     public required init(dependencyContainer: Container) {
         self.dependencyContainer = dependencyContainer
     }
     
-    public func presentable(for route: {PROJECT}Route) -> Presentable {
+    public func presentable(for route: {PROJECT}Module.Route) -> Presentable {
         switch route {
         case .show:
+            let viewController = StoryboardScene.{PROJECT}Scenes.initialScene.instantiate()
+            let context = {PROJECT}Context(
+                initialState: {PROJECT}State(),
+                dependencyContainer: dependencyContainer
+            )
+            viewController.context = context
+            return Presentable(viewController: viewController)
         }
     }
     
-    public static func route(for userActivity: NSUserActivity) -> {PROJECT}Route? {
-        return {PROJECT}Route.show
+    public static func route(for userActivity: NSUserActivity) -> {PROJECT}Module.Route? {
+        return {PROJECT}Module.Route.show
     }
 }
